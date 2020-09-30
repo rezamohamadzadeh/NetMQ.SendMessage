f25e64ec42d0d0d0e5c6c80f461570a30a0a18a7
Administrator <Administrator@DESKTOP-6IC4J3S>
2020-09-30 09:56:41 +0330

    Add project files.

---
+++ NetMQ.SendMessage/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NetMQ.SendMessage.Models;
+using NetMQ.Sockets;
+
+namespace NetMQ.SendMessage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Privacy(string message)
+        {
+            using var client = new RequestSocket("tcp://localhost:8050");
+
+            System.Console.WriteLine($"Sending message '{message}'...");
+
+            client.SendFrame(message);
+            //System.Console.WriteLine("Message has been sent.");
+
+            List<string> messages = client.ReceiveMultipartStrings(2);
+            //foreach (string message in messages)
+            //{
+            //    System.Console.WriteLine($"Server response: {message}");
+            //}
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
